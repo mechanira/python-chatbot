366a82fe3904bbb074112ba7f3d90a80c22bbb4d
mechanira <marklegend293@gmail.com>
2023-05-22 15:48:52 +0200

    Initial setup

---
+++ src/chatbot.py
@@ -0,0 +1,82 @@
+import random
+import json
+import datetime
+import pickle
+import numpy as np
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+from keras.models import load_model
+
+lemmatizer = WordNetLemmatizer()
+intents = json.loads(open('intents.json').read())
+
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+model = load_model('chatbotmodel.h5')
+
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+    return sentence_words
+
+def bag_of_words(sentence):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for w in sentence_words:
+        for i, word in enumerate(words):
+            if word == w:
+                bag[i] = 1
+    return np.array(bag)
+
+def predict_class(sentence):
+    bow = bag_of_words(sentence)
+    res = model.predict(np.array([bow]), verbose=0)[0]
+    ERROR_THRESHOLD = 0.0
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
+    return return_list
+
+
+def time(response):
+    current_time = datetime.datetime.now().strftime("%H:%M:%S")
+    response = response.replace("{time}", current_time)
+    return response
+
+
+mapping = {
+    "time": time
+}
+
+
+def get_response(intents_list, intents_json):
+    tag = intents_list[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            result = random.choice(i['responses'])
+            handler = mapping.get(tag)  # Get the corresponding function handler from the mapping
+            if handler is not None:
+                result = handler(result)  # Pass the response to the function for further processing
+            break
+    return result
+
+print("Bot is running")
+DEBUG_MODE = True
+
+
+while True:
+    message = input("\nYou: ")
+    ints = predict_class(message)
+    res = get_response(ints, intents)
+    print("Bot: " + res)
+
+    if DEBUG_MODE:
+        probability = ints[0]['probability']
+        print(f"DEBUG: From intent '{ints[0]['intent']}' with a probablity of {float(probability) * 100} %")
